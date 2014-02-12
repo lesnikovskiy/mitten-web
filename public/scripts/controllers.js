51a9789ed2f387b74926600edf502e44aa3ffcd4
lesnikovskiy <lesnikovski@gmail.com>
2014-02-12 18:11:15 +0200

    client side started

--- public/scripts/controllers.js
+++ public/scripts/controllers.js
@@ -1,148 +1,47 @@
 var mittenApp = angular.module('mittenApp', []);
 
 mittenApp.controller('CurrentConditionCtrl', function CurrentConditionCtrl($scope, $http, $window) {
-	$scope.location = [];
-	$scope.getWeather = function () {
-		
-	};
-	function getWeather() {
-		$http.get('api/weather/comparable').success(function (data, status, headers) {
-			if (data.ok && data.data && data.data) {
-				$scope.currentState = data.data.current_condition;
-				$scope.currentState.time = new Date(data.data.current_condition.observation_time).toString();
-				$scope.content = {
-					condition: true,
-					error: false,
-					login: false, 
-					register: false
-				}
-			} else if (!data.ok && data.type == 'unauthorized') {
-				$scope.content = {
-					condition: false,
-					error: false,
-					login: true, 
-					register: false
-				}
-			} else {
-				$scope.error = {
-					message: data.error.message
-				};
-				$scope.content = {
-					condition: true,
-					error: true,
-					login: false, 
-					register: false
-				};
-			}
-		});
+	$scope.locationError;
+	$scope.data = {};
+	
+	function handleLocationError(err) {
+		debugger;
+		switch (error.code) {
+			case 0:
+				$scope.locationError = 'There was an error while retrieving your location. Additional details: ' + error.message;
+				break;
+			case 1:
+				$scope.locationError = 'The user opted not to share his or her location.';
+				break;
+			case 2:
+				$scope.locationError = 'The browser was unable to determine your location. Additional details: ' + error.message;
+				break;
+			case 3:
+				$scope.locationError = 'The browser timed out before retrieving the location.';
+				break;
+		}
 	}
 	
-	getWeather();
-	
-	$scope.login = function() {
-		$http.post('/api/login', {email: $scope.email, password: $scope.password}).success(function(data) {
-			if (data.ok) {
-				getWeather();
-			} else {
-				$scope.content = {
-					condition: false,
-					error: true,
-					login: true, 
-					register: false
-				};
-				$scope.error = {
-					message: data.error.message
-				};
-			}
-		});
-	};
-	
-	$scope.showRegister = function() {
-		$scope.content = {
-			condition: false,
-			error: false,
-			login: false, 
-			register: true
-		};
-	};
-	
-	$scope.register = function() {
-		$.mobile.loading('show');
-		navigator.geolocation.getCurrentPosition(function (pos) {
-			if (pos.coords) {
-				var lat = pos.coords.latitude;
-				var lng = pos.coords.longitude;
-				$http.post('/api/hip', {
-					email: $scope.email,
-					password: $scope.password,
-					location: { lat: lat, lng: lng }
-				}).success(function (data) {
-					if (data.ok) {
-						getWeather();
-					} else {
-						$scope.content = {
-							condition: false,
-							error: true,
-							login: false, 
-							register: true
-						};
-						$scope.error = {
-							message: data.error.message || 'unkown error'
-						};
-					}
-					$.mobile.loading('hide');
+	$scope.getWeather = function () {
+		$window.navigator.geolocation.getCurrentPosition(function (pos) {
+			var url = '/api/weather/' + pos.coords.latitude + '/' + pos.coords.longitude;
+			$http.get(url)
+				.success(function (data, status, headers, config) {
+					debugger;
+					if (data.data)
+						$scope.data = data;
+				})
+				.error(function (data, status, headers, config) {
+				
 				});
-			} else {
-				$scope.content = {
-					condition: false,
-					error: true,
-					login: false, 
-					register: true
-				};
-				$scope.error = {
-					message: 'Coordinates not found'
-				};
-				$.mobile.loading('hide');
-			}
-		}, function (err) {
-			$scope.content = {
-				condition: false,
-				error: true,
-				login: false, 
-				register: true
-			};
-			switch (err.code) {
-				case 0:
-					$scope.error = {
-						message: 'There was an error while retrieving your location: ' + err.message
-					};
-					$.mobile.loading('hide');
-					break;
-				case 1:
-					$scope.error = {
-						message: 'The location is turned off. Please turn on location in your browser settings. Error details: ' + err.message
-					};
-					$.mobile.loading('hide');
-					break;
-				case 3:
-					$scope.error = {
-						message: 'The browser timed out before retrieving the location.'
-					};
-					$.mobile.loading('hide');
-					break;
-				default:
-					$scope.error = {
-						message: 'Unknown geolocation error.'
-					};
-					$.mobile.loading('hide');
-					break;
-			}
-		});
-	};
+		}, handleLocationError);
+	};		
 	
 	$scope.logout = function() {
-		$http.post('/api/logout').success(function (data) {
-			getWeather();
+		$http.get('/auth/facebook/logout').success(function (data) {
+			//getWeather();
 		});
 	};
+	
+	$scope.getWeather();
 });
